1f176527c512d2dcff2d2986680a826f84f1d59e
Gopakumar k <gopakumark00@gmail.com>
2021-05-17 12:50:04 +0530

    Add project files.

---
+++ EcommerceAPI/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceAPI.Data;
+using EcommerceAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private  StoreContext _context { get; }
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _context = storeContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+
+
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
